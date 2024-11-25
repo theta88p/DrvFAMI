96afa88476c9fbc59b9e732ca40007686683c6a7
theta88p <theta88p@gmail.com>
2024-11-25 18:48:29 +0900

    ドライバのコピー先にFamiDriverを追加

--- drv/_build_copy.bat
+++ drv/_build_copy.bat
@@ -5,6 +5,8 @@ set CC65_HOME=c:\cc65
 set PATH=%CC65_HOME%\bin;%MSYS_HOME%\usr\bin
 set DBG_DIR=..\DrvFAMICompiler\bin\Debug\bin
 set REL_DIR=..\DrvFAMICompiler\bin\Release\bin
+set WS_DBG_DIR=..\..\FamiDriver\FamiDriver\bin\x64\Debug\net8.0-windows7.0\bin
+set WS_REL_DIR=..\..\FamiDriver\FamiDriver\bin\x64\Release\net8.0-windows7.0\bin
 
 del buildlog.txt
 del errlog.txt
@@ -13,6 +15,12 @@ make -k >buildlog.txt 2>&1
 if %errorlevel% equ 0	goto end
 start errlog.txt
 :end
+copy /y drv.bin %WS_DBG_DIR%
+copy /y drv_*.bin %WS_DBG_DIR%
+copy /y dsp_*.bin %WS_DBG_DIR%
+copy /y drv.bin %WS_REL_DIR%
+copy /y drv_*.bin %WS_REL_DIR%
+copy /y dsp_*.bin %WS_REL_DIR%
 copy /y drv.bin %REL_DIR%
 copy /y drv_*.bin %REL_DIR%
 copy /y dsp_*.bin %REL_DIR%
