2693a2d95ddecd2707b5124efffa7f36901bad63
theta88p <theta88p@gmail.com>
2025-01-09 02:59:47 +0900

    ドライバのコピー先を追加

--- drv/_build_copy.bat
+++ drv/_build_copy.bat
@@ -5,8 +5,9 @@ set CC65_HOME=c:\cc65
 set PATH=%CC65_HOME%\bin;%MSYS_HOME%\usr\bin
 set DBG_DIR=..\DrvFAMICompiler\bin\Debug\bin
 set REL_DIR=..\DrvFAMICompiler\bin\Release\bin
-set WS_DBG_DIR=..\..\FamiDriver\FamiDriver\bin\x64\Debug\net8.0-windows7.0\bin
-set WS_REL_DIR=..\..\FamiDriver\FamiDriver\bin\x64\Release\net8.0-windows7.0\bin
+set WS_DBG_DIR=..\..\FamiDriver\FamiDriver\bin\x64\Debug\net9.0-windows7.0\bin
+set WS_REL_DIR=..\..\FamiDriver\FamiDriver\bin\x64\Release\net9.0-windows7.0\bin
+set WS_PRJ_DIR=..\..\FamiDriver\bin
 
 del buildlog.txt
 del errlog.txt
@@ -15,6 +16,9 @@ make -k >buildlog.txt 2>&1
 if %errorlevel% equ 0	goto end
 start errlog.txt
 :end
+copy /y drv.bin %WS_PRJ_DIR%
+copy /y drv_*.bin %WS_PRJ_DIR%
+copy /y dsp_*.bin %WS_PRJ_DIR%
 copy /y drv.bin %WS_DBG_DIR%
 copy /y drv_*.bin %WS_DBG_DIR%
 copy /y dsp_*.bin %WS_DBG_DIR%
@@ -24,6 +28,6 @@ copy /y dsp_*.bin %WS_REL_DIR%
 copy /y drv.bin %REL_DIR%
 copy /y drv_*.bin %REL_DIR%
 copy /y dsp_*.bin %REL_DIR%
-move /y drv.bin %DBG_DIR%
-move /y drv_*.bin %DBG_DIR%
-move /y dsp_*.bin %DBG_DIR%
+copy /y drv.bin %DBG_DIR%
+copy /y drv_*.bin %DBG_DIR%
+copy /y dsp_*.bin %DBG_DIR%
