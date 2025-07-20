79a0387b62e63d3e99d359574807fe2538418922
theta88p <theta88p@gmail.com>
2025-07-21 05:41:23 +0900

    FDS音源デバッグ用の改変

---
+++ drv/data_nsf_dbg.s
@@ -0,0 +1,17 @@
+.export _BGM0
+.export _DPCMinfo
+
+.segment "MUSDATA"
+
+_BGM0:
+	;.byte	"SEQ"
+	.incbin "test.bin"
+
+.segment "PCMDATA"
+
+_DPCMinfo:
+	.byte	"DPCM"
+	;.incbin	"C:\Programs\mck\Mumml\prog\dmc\tight909kick_v60.dmc"
+	;.incbin "C:\Programs\mck\Mumml\prog\dmc\cowbell.dmc"
+
+.segment "CHARS"
